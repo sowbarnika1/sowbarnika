65785f0a3b443a567e82018a6b57e51a15f655ff
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2018-12-22 19:20:13 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,21 @@
+int main()
+{
+char n;
+scanf("%c",&n);
+if(n>='a'||n<='z' && n>='A'||n<='Z')
+{
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+    {
+printf("vowel");
+    }
+else 
+    {
+printf("consonant");
+    }
+}
+else
+{
+printf("invalid");
+}
+return 0;
+}
