df683c4613636e892830714ca074faddebedb963
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2018-12-23 18:00:20 +0530

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,19 @@
+int main()
+{
+    char a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+    printf("a is greater");    
+    }
+    else if(b>a&&b>c)
+    {
+        printf("b is greater");
+    }
+    else
+    {
+        printf("c is greater ");
+    }
+
+    return 0;
+}
