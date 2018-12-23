b4c4b7ad039494deb36f632fdb14571606f8ce13
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2018-12-23 17:49:08 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+    {
+        printf("alphabet");
+    }
+    else
+    {
+        printf("not alphabet");
+    }
+
+    return 0;
+}
