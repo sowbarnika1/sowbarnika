c65a4fc525c97ba66c5e38630a714ef9b54013b8
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2018-12-23 18:06:50 +0530

    Create set17.c

---
+++ set17.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%4==0 || a%400==0 && a%100!=0)
+    {
+        printf("leap year");
+    }
+    else
+    {
+        printf("not leap year");
+    }
+     return 0;
+}
