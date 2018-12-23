8eae1e94968d7fe7d00d4da7cb7f247fb0de3bd7
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2018-12-23 18:51:58 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,7 +2,7 @@ int main()
 {
 char n;
 scanf("%c",&n);
-if(n>='a'||n<='z' && n>='A'||n<='Z')
+if(n>='a'&&n<='z' || n>='A'&&n<='Z')
 {
 if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
     {
