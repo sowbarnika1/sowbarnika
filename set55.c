945ecfde20b971b8b62317baec8ea23cf999ebd4
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-01-26 20:28:41 +0530

    Update set55.c

--- set55.c
+++ set55.c
@@ -3,11 +3,14 @@ int main()
     long int n;
     int c=0;
     scanf("%ld",&n);
+    if(n>=0 && n<=100000000000)
+    {
     while(n>0)
     {
         n=n/10;
         c++;
     }
     printf("%d",c);
+    }
     return 0;
 }
