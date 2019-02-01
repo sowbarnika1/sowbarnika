464a7c54ef450ef303026034be155d5f4e3608aa
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-01 20:38:36 +0530

    Create set65.c

---
+++ set65.c
@@ -0,0 +1,15 @@
+int main()
+{
+     int n,m;
+    scanf("%d %d",&n,&m);
+    n=n+m;
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
