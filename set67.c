273280360fc570ce8e74373d4f240ea390f686fb
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-02 20:22:17 +0530

    Create set67.c

---
+++ set67.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int a[10],n,k,i,c=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
