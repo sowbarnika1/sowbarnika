bd2311c63b3cfba60af50fee335e75dd1e003d0c
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-02 20:02:52 +0530

    Create set66.c

---
+++ set66.c
@@ -0,0 +1,26 @@
+int main()
+{
+    char a[20];
+ int i,flag=0,c=0;
+ scanf("%s",a);
+ for(i=0;a[i]!='\0';i++)
+ {
+     if((a[i]>='a' && a[i]<='z')||(a[i]>='A' && a[i]<='Z'))
+     {
+         flag=1;
+     }
+     if(a[i]>='0' && a[i]<='9')
+     {
+         c=1;
+     }
+ }
+ if(flag==1 && c==1)
+ {
+     printf("yes");
+ }
+ else
+ {
+     printf("no");
+ }
+return 0;
+}
